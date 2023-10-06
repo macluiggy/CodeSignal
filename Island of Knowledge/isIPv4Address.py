9b3b88e99057ef7f7ab7ef45f4fa4c39c3283379
macluiggy <luiggy.macias@imagineapps.co>
2023-10-05 21:07:49 -0500

    add test and ignoring files

--- Island of Knowledge/isIPv4Address.py
+++ Island of Knowledge/isIPv4Address.py
@@ -1,5 +1,5 @@
 def solution(inputString):
-    print(inputString)
+    print('this is not from root')
     parts = inputString.split('.')
     
     if len(parts) != 4:
@@ -13,10 +13,3 @@ def solution(inputString):
             return False
         if part[0] == '0' and len(part) > 1:
             return False
-
-print(solution('172.16.254.1'))
-# Test cases
-assert solution('172.16.254.1') == True
-assert solution('172.316.254.1') == True
-assert solution('.254.255.0') == False
-assert solution('280.100.100.100') == False
---
+++ Island of Knowledge/test/test_isIPv4Address.py
@@ -0,0 +1,16 @@
+import pytest
+from isIPv4Address import solution
+
+def test_valid_ipv4_addresses():
+    assert solution("172.16.254.1") == True
+    assert solution("192.0.2.0") == True
+    assert solution("0.0.0.0") == True
+    assert solution("255.255.255.255") == True
+
+def test_invalid_ipv4_addresses():
+    assert solution("") == False
+    assert solution("172.16.254.01") == False
+    assert solution("172.16.254.1.1") == False
+    assert solution("172.16.254") == False
+    assert solution("172.16.254.abc") == False
+    assert solution("172.16.254.256") == False
