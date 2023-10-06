05af5580d3888f29bdf45afbed7644b464fa73c1
macluiggy <luiggy.macias@imagineapps.co>
2023-10-05 20:18:16 -0500

    array maximal adjancent difference

---
+++ Island of Knowledge/arrayMaximalAdjacentDifference.py
@@ -0,0 +1,13 @@
+def solution(inputArray):
+    # store the maximum difference starting with 0
+    maximumDifference = 0
+    # iterate through the array
+    for i in range(len(inputArray) - 1):
+        # get the difference between the current and the next element
+        difference = abs(inputArray[i] - inputArray[i + 1])
+        # if the difference is greater than the current maximum difference
+        if difference > maximumDifference:
+            # store the difference as the maximum difference
+            maximumDifference = difference
+    # return the maximum difference
+    return maximumDifference
