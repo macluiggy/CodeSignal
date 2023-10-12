73d1c2a4f2ced7dcc61401c12d6198d691091eb1
macluiggy <luiggy.macias@imagineapps.co>
2023-10-12 16:16:36 -0500

    sjkfdjf

--- Island of Knowledge/Minesweeper.js
+++ Island of Knowledge/Minesweeper.js
@@ -5,15 +5,14 @@ function solution(matrix) {
     .fill()
     .map(() => Array(numCols).fill(0));
 
-  const vecinos1 = [-1, 0, 1];
-  const vecinos2 = [-1, 0, 1];
+  const rowIndexOffsets = [-1, 0, 1];
   for (let i = 0; i < newMatrix.length; i++) {
     for (let j = 0; j < newMatrix[i].length; j++) {
       let minesCounterInJ = 0;
-      for (let k = 0; k < vecinos1.length; k++) {
-        const h = vecinos1[k];
-        for (let l = 0; l < vecinos2.length; l++) {
-          const v = vecinos2[l];
+      for (let k = 0; k < rowIndexOffsets.length; k++) {
+        const h = rowIndexOffsets[k];
+        for (let l = 0; l < rowIndexOffsets.length; l++) {
+          const v = rowIndexOffsets[l];
           const x = i + h;
           const y = j + v;
           // i was stuck here because i was not considering the case when h and v are both 0
